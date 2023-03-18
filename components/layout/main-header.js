b7c0abfd7ef40c4bc4dac239e802db4f7729d71d
millana_chotubaeva <chotubaevam@gmail.com>
2023-03-17 22:19:51 -0700

    * add layout component for nav bar

---
+++ components/layout/main-header.js
@@ -0,0 +1,21 @@
+import { Main } from "next/document";
+import Link from "next/link";
+import classes from "./main-header.module.css";
+
+function MainHeader() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <Link href="/">Next Events</Link>
+      </div>
+      <nav className={classes.navigation}>
+        <ul>
+          <li>
+            <Link href="/events">Browse All Events</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default MainHeader;
--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,4 +1,7 @@
+import { getAllEvents } from "../../../dummy-data";
+
 function AllEventsPage() {
+    const events = getAllEvents()
   return (
     <div>
       <h1>Events Page</h1>
